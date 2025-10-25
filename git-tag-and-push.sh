d49a51487048bc9b0b351ed9f76f166c6796512c
Ahad Chowdhury <ahad@maacpiash.com>
2025-10-25 13:51:11 +1100

    Update script to take custom tag from command line

--- git-tag-and-push.sh
+++ git-tag-and-push.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
-TAG=$(TZ="Australia/Sydney" date +"%Y.%m.%d.%H%M")
+if [ -n "$1" ]; then
+	TAG="$1"
+else
+	TAG=$(TZ="Australia/Sydney" date +"%Y.%m.%d.%H%M")
+fi
+
 git tag "$TAG"
 git push github "$TAG"
